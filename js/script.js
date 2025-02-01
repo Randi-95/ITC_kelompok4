7530f0d80e44cd36c58fe2c81544914a2623239f
Randi-95 <randiganteng9528@gmail.com>
2025-02-01 15:59:15 +0700

    update login & register

---
+++ js/script.js
@@ -0,0 +1,31 @@
+const hamburger = document.querySelector('.hamburger');
+const menuNav = document.querySelector('.menu');
+const closed = document.querySelector('.close');
+
+hamburger.addEventListener('click', function(){
+    menuNav.classList.add('right-0')
+})
+
+closed.addEventListener('click', function(){
+    menuNav.classList.remove('right-0')
+})
+
+const navbar = document.querySelector('.navbar');
+
+window.addEventListener('scroll', function(){
+    const windowScrool = window.scrollY > 0;
+    navbar.classList.toggle('scrolling-active', windowScrool);
+})
+
+const closeLogin = document.querySelector('.button-login')
+const login = document.querySelector('.login')
+const getStarted = document.querySelector('.get-started')
+
+closeLogin.addEventListener('click', function(){
+    login.classList.add('hidden')
+})
+
+getStarted.addEventListener('click', function(){
+    login.classList.remove('hidden')
+})
+
