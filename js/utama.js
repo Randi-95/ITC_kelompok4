cae98ab549eff9a237c9d32fce3bbccf69cfe0d4
Randi-95 <randiganteng9528@gmail.com>
2025-02-15 16:42:51 +0700

    hover

--- js/utama.js
+++ js/utama.js
@@ -8,13 +8,37 @@ buttonDropdown.addEventListener('click', function(){
 const usernameProfile = document.querySelector('.username-profile')
 const isiProfile = document.querySelector('.isi-profile')
 
-usernameProfile.addEventListener('click', function(){
-    isiProfile.classList.toggle('hidden')
+usernameProfile.addEventListener('mouseover', function(){
+    isiProfile.classList.remove('hidden')
+})
+
+usernameProfile.addEventListener('mouseout', function(){
+    isiProfile.classList.add('hidden')
+})
+
+isiProfile.addEventListener('mouseover', function(){
+    isiProfile.classList.remove('hidden')
+})
+
+isiProfile.addEventListener('mouseout', function(){
+    isiProfile.classList.add('hidden')
 })
 
 const buttonNotifikasi = document.querySelector('.notifikasi')
 const isiNotifikasi = document.querySelector('.isi-notifikasi')
 
-buttonNotifikasi.addEventListener('click', function(){
-    isiNotifikasi.classList.toggle('hidden')
+buttonNotifikasi.addEventListener('mouseover', function(){
+    isiNotifikasi.classList.remove('hidden')
+})
+
+buttonNotifikasi.addEventListener('mouseout', function(){
+    isiNotifikasi.classList.add('hidden')
+})
+
+isiNotifikasi.addEventListener('mouseover', function(){
+    isiNotifikasi.classList.remove('hidden')
+})
+
+isiNotifikasi.addEventListener('mouseout', function(){
+    isiNotifikasi.classList.add('hidden')
 })
