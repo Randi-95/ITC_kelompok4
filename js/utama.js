ef04cfc92b23260748d632ea6147fe823a62631a
Randi-95 <randiganteng9528@gmail.com>
2025-02-15 07:29:48 +0700

    done minus responsice

--- js/utama.js
+++ js/utama.js
@@ -4,3 +4,14 @@ const topikForum = document.querySelector('.topik-forum')
 buttonDropdown.addEventListener('click', function(){
     topikForum.classList.toggle('active')
 })
+
+const usernameProfile = document.querySelector('.username-profile')
+const isiProfile = document.querySelector('.isi-profile')
+
+usernameProfile.addEventListener('mouseover', function(){
+    isiProfile.classList.remove('hidden')
+})
+
+usernameProfile.addEventListener('mouseout', function(){
+    isiProfile.classList.add('hidden')
+})
